d8836058c8d80d6aba6862b94a9fea291c5b9a65
Kibrewossen Tesera <47031138+kebtes@users.noreply.github.com>
2025-07-02 20:59:38 +0300

    predictionService.js

--- frontend/js/predictionService.js
+++ frontend/js/predictionService.js
@@ -33,7 +33,7 @@ export async function sendCanvasForPrediction(canvas) {
     startPredictingAnimation();
     
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://doodle-predict.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
